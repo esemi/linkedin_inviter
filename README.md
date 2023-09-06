132e1659c7fbc35a7deb3cadec0b476e2c2a0927
Simon <spam@esemi.ru>
2023-09-06 13:16:33 +0200

    :fix: work on server

--- README.md
+++ README.md
@@ -4,19 +4,19 @@ Linkedin inviter.
 
 
 ### Local setup
-```shell
-$ git clone git@github.com:esemi/linkedin_inviter.git
-$ cd linkedin_inviter
-$ python3.11 -m venv venv
-$ source venv/bin/activate
-$ pip install -U poetry
-$ poetry install
+```bash
+git clone git@github.com:esemi/linkedin_inviter.git
+cd linkedin_inviter
+python3.11 -m venv venv
+source venv/bin/activate
+pip install -U poetry
+poetry install
 ```
 
 ### Run linters
 ```bash
-$ poetry run mypy app/
-$ poetry run flake8 app/
+poetry run mypy app/
+poetry run flake8 app/
 ```
 
 ### Run inviter
--- app/selenium_actions.py
+++ app/selenium_actions.py
@@ -10,8 +10,19 @@ from app.settings import app_settings
 
 def create_browser() -> WebDriver:
     """Create browser."""
+    options = [
+        f'user-data-dir={app_settings.session_path}',
+        'start-maximized',
+        'disable-infobars',
+        '--disable-extensions',
+        '--disable-dev-shm-usage',
+        '--no-sandbox',
+        '--headless',
+    ]
     chrome_options = Options()
-    chrome_options.add_argument(f'user-data-dir={app_settings.session_path}')
+    for option in options:
+        chrome_options.add_argument(option)
+
     browser = webdriver.Chrome(options=chrome_options)
     browser.maximize_window()
     browser.implicitly_wait(app_settings.timeout_default)
